a5a4d0ee8d5034222e9ea637f0ffd01eb6c4cde0
scorley3 <scorley3@illinois.edu>
2023-02-09 14:47:30 -0600

    Add buttons for edit category

--- timeManager.js
+++ timeManager.js
@@ -36,7 +36,8 @@ function createSection(event) {
 	catDiv.style.margin = "0px";
 	catDiv.style.padding = "0px";
 	catDiv.style.borderWidth = "0px";
-	div.id = "category" + event.target.id.substring(12);
+	var categoryNumber = event.target.id.substring(12);
+	div.id = "category" + categoryNumber;
 	catIdArray.push(div.id);
 	console.log(div.id);
 	var title = document.createElement("h3");
@@ -45,8 +46,54 @@ function createSection(event) {
 	var textAr = document.createElement("p");
 	textAr.innerHTML = "example text example text";
 	div.appendChild(textAr);
+	var buttonsDiv = document.createElement("div");
+	buttonsDiv.id = "buttonsDiv" + categoryNumber;
+	var editButton = document.createElement("input");
+	editButton.type = "button";
+	editButton.value = "Edit Category";
+	editButton.id = "editButton" + categoryNumber;
+	editButton.addEventListener("click", editCategory);
+	var addButton = document.createElement("input");
+	addButton.type = "button"
+	addButton.value = "Add Task";
+	addButton.id = "addButton" + categoryNumber;
+	buttonsDiv.appendChild(editButton);
+	buttonsDiv.appendChild(addButton);
+	div.appendChild(buttonsDiv);
 	document.getElementById("dashboard").appendChild(div);
-	
-	
+}
+function editCategory(event) {
+	categoryNumber = event.target.id.substring(10);
+	if (document.getElementById("editDiv" + categoryNumber) == null) {
+		console.log(categoryNumber);
+		var catDiv = document.getElementById("category" + categoryNumber);
+		var editDiv = document.createElement("div");
+		editDiv.id = "editDiv" + categoryNumber;
+		var newTitle = document.createElement("input");
+		newTitle.type = "text";
+		newTitle.id = "editTitle" + categoryNumber;
+		var titleLabel = document.createElement("label");
+		titleLabel.innerHTML = "New Title";
+		titleLabel.htmlFor = newTitle.id;
+		var newDescription = document.createElement("input");
+		newDescription.type = "text";
+		newTitle.id = "editDesc" + categoryNumber;
+		var descLabel = document.createElement("label");
+		descLabel.innerHTML = "New Description";
+		descLabel.htmlFor = newDescription.id;
+		var saveButton = document.createElement("input");
+		saveButton.type = "button";
+		saveButton.value = "Save";
+		saveButton.id = "saveButton" + categoryNumber;
+		editDiv.appendChild(titleLabel);
+		editDiv.appendChild(newTitle);
+		editDiv.appendChild(descLabel);
+		editDiv.appendChild(newDescription);
+		editDiv.appendChild(saveButton);
+		catDiv.appendChild(editDiv);
+	}
 	
 }
+//in each section, what do we want? 
+//ability to add/edit title and description 
+//ability to add a task 
